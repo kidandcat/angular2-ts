7d2789235d08eaae280c65fa4d69e99e293cc4b4
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-05-07 22:06:08 +0200

    'fixes'

--- app/main.js
+++ app/main.js
@@ -1,4 +1,3 @@
-/// <reference path="../node_modules/angular2/typings/tsd.d.ts" />
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/angular2/typings/tsd.d.ts" />
+
 
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
